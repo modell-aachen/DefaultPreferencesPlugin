70fa74b401563e37577df77849f4f04c5f1928b7
Andre Tebart <tebart@modell-aachen.de>
2017-02-21 10:44:33 +0100

    Adds new overview feature to WebPreferences and SitePreferences. WebPreferences can now be inherited from other webs.
    id:352
    
    Ticket: none
    
    Release-Note(feature,poweruser){
    WebPreferences and SitePreferences now show an overview of all active Preferences. These preferences can be filtered by name or value.
    They also show the inheritance path which enables Key Users to easily comprehend where preferences originate from. Additionally it is now
    possible to inherit WebPreferences from other Webs. See the documentation of the DefaultPreferencesPlugin for further details.
    }

---
+++ dev/components/DefaultPreferences.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="flatskin-wrapped">
+<div class="expanded row">
+    <div class="columns">
+    <label>Filter
+        <input type="text" v-model="filter">
+    </label>
+    </div>
+    <fieldset v-if="!isSitePrefTopic" class="shrink columns">
+    <legend>Options</legend>
+    <input id="sitePrefsOnly" type="checkbox" v-model="showSitePrefsOnlySettings"><label for="sitePrefsOnly">Include SitePreferences only settings</label>
+  </fieldset>
+</div>
+    <preference v-for="pref in filteredPreferences" v-bind:key="pref.name" :preference="pref">
+</div>
+</template>
+
+<script>
+/* global $ */
+import Preference from './Preference.vue';
+export default {
+    data(){
+        return {
+            prefs: {},
+            filter: "",
+            isSitePrefTopic: false,
+            showSitePrefsOnlySettings: false,
+        };
+    },
+    components: {
+        Preference
+    },
+    props: ["preferencesSelector"],
+    computed: {
+        filteredPreferences() {
+            let self = this;
+            return this.prefs.filter((pref) => {
+                if(!self.showSitePrefsOnlySettings && pref.inheritPath[pref.inheritPath.length - 1].source === "Main/SitePreferences"){
+                    return false;
+                }
+                if(!this.filter)
+                    return true;
+                let tester = new RegExp(this.filter.replace(/([.?*+^$[\]\\(){}|-])/g, "\\$1"), 'gi');
+                return tester.test(pref.name) || tester.test(pref.value);
+            }).sort((a, b) => {
+                return a.name.localeCompare(b.name);
+            });
+        }
+    },
+    created() {
+        let config = JSON.parse($("." + this.preferencesSelector).html());
+        this.prefs = config.preferences;
+        this.isSitePrefTopic = config.isSitePrefTopic;
+        this.showSitePrefsOnlySettings = this.isSitePrefTopic;
+    }
+};
+</script>
---
+++ dev/main.js
@@ -0,0 +1,12 @@
+import Vue from 'vue'
+import DefaultPreferences from './components/DefaultPreferences.vue'
+
+
+$( function () {
+    new Vue({
+        el: '#defaultPrefs',
+        components: {
+            DefaultPreferences
+        }
+    })
+})
