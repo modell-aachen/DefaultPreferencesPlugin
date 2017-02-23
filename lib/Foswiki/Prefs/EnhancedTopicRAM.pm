514bf4e3b205da77407c00ade1e9dbed875ef754
Andre Tebart <tebart@modell-aachen.de>
2017-02-23 13:39:17 +0100

    Fixes INHERITED_WEB not working when the value has trailing whitespace.
    Ticket: none
    
    Release-Note(){
    }

--- lib/Foswiki/Prefs/EnhancedTopicRAM.pm
+++ lib/Foswiki/Prefs/EnhancedTopicRAM.pm
@@ -24,6 +24,8 @@ sub new {
     # The only way to check this is to inspect the calling function.
     # On a push to the topic context the constructor is always called by Foswiki::Prefs::_getBackend.
     my $inheritedWeb = $this->{values}{INHERITED_WEB};
+    $inheritedWeb =~ s/\s*//g;
+
     my (undef, undef, undef, $subroutine) = caller(1);
     if($inheritedWeb && $subroutine eq 'Foswiki::Prefs::_getBackend'){
       my $prefs = $meta->{_session}->{prefs};
