21e51b4f7ccb66a6f3be57dff80ce477fae1c054
Daniel Supplieth <supplieth@modell-aachen.de>
2017-05-03 16:08:39 +0200

    Copy preferences to clipboard with three spaces.
    Ticket: none
    Release-Note(fix, poweruser){
        Default preferences will now be copied to clipboard with three
        leading spaces.
    }

--- dev/components/Preference.vue
+++ dev/components/Preference.vue
@@ -33,7 +33,7 @@ export default {
     props: ["preference"],
     computed: {
         clipboardText() {
-            return `* Set ${this.preference.name} = ${this.preference.value}`;
+            return `   * Set ${this.preference.name} = ${this.preference.value}`;
         }
     },
     mounted() {
